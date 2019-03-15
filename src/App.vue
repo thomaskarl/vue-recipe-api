7f1aa3c5a52a169d2050f6782a5dccccf42f533a
Thomas Karl <thomaskarlandersson@outlook.com>
2019-03-15 13:37:08 +0100

    Changes done by Fredrik

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-	<div id="app">
-		<div v-for="recipe in recipes.results">
-			<Recipe v-bind:title="recipe.title"
-					v-bind:website="recipe.href"
-					v-bind:ingredients="recipe.ingredients"
-					v-bind:recImg="recipe.thumbnail"
-			></Recipe>
+	<div>
+		<div v-if="loaded">
+			<div v-for="recipe in recipes.results">
+				<Recipe v-bind:title="recipe.title"
+						v-bind:website="recipe.href"
+						v-bind:ingredients="recipe.ingredients"
+						v-bind:recImg="recipe.thumbnail"
+				></Recipe>
+			</div>
+		</div>
+		<div v-else>
+			<p>fetcing data</p>
+			<button @click="beforeMount">Last tingene</button>
 		</div>
 	</div>
 </template>
@@ -20,22 +26,29 @@
 		},
 		data() {
 			return {
+				loaded: false,
 				recipes: []
 			}
 		},
-		beforeMount: function () {
-			const app = this;
 
-			const conversionUrl = 'https://cors-anywhere.herokuapp.com/';
-			const url = 'http://www.recipepuppy.com/api/';
+		mounted() {
+			// this.beforeMount();
+		},
 
-			fetch(conversionUrl + url)
-				.then(function (response) {
-					return response.json();
-				})
-				.then(function (result) {
-					app.recipes = result;
-				})
+		methods: {
+			beforeMount: function () {
+				const conversionUrl = 'https://cors-anywhere.herokuapp.com/';
+				const url = 'http://www.recipepuppy.com/api/';
+
+				fetch(conversionUrl + url)
+					.then((response) => {
+						return response.json();
+					})
+					.then((result) => {
+						this.loaded = true;
+						this.recipes = result;
+					})
+			}
 		}
 	}
 
